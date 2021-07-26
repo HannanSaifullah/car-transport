add33946498f16c96bf77f5ae5a6565224ce5d40
Hannan Saifullah <47532981+HannanSaifullah@users.noreply.github.com>
2021-07-26 20:04:45 +0530

    Header update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,11 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
+  styleUrls:['header.style.scss']
 })
 export class HeaderComponent  {
-    @Input() title: string;
+    @Input() title: string = 'Easy Car Transport';
 
     settings() {
 
+    }
+
+    ngOnInit() {
     }
 }
